944ffba082b53ed3e45713843417c611826620a6
Taylor Monacelli <taylormonacelli@gmail.com>
2024-01-17 08:54:08 -0800

    remove dependencies that are already pulled in transitively

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test('test', async ({ page }) => {
   const USERNAME = process.env.SEATTLE_UTILITIES_USERNAME;
