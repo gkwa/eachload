565a62467b075dcbc1532c57c3d7d17daacc0ef9
Taylor Monacelli <taylormonacelli@gmail.com>
2024-01-18 07:58:27 -0800

    hold off on deploying secrets only when we need them

--- ci/main.go
+++ ci/main.go
@@ -25,14 +25,9 @@ func main() {
 	}
 	defer client.Close()
 
-	secret_user := client.SetSecret("utilities-username-secret", os.Getenv("SEATTLE_UTILITIES_USERNAME"))
-	secret_pass := client.SetSecret("utilities-password-secret", os.Getenv("SEATTLE_UTILITIES_PASSWORD"))
-
 	source := client.Container().
 		From("mcr.microsoft.com/playwright:v1.41.0-jammy").
 		WithExec([]string{"npm", "install", "-g", "npm@latest"}).
-		WithSecretVariable("SEATTLE_UTILITIES_USERNAME", secret_user).
-		WithSecretVariable("SEATTLE_UTILITIES_PASSWORD", secret_pass).
 		WithDirectory("/src", client.Host().Directory("."), dagger.ContainerWithDirectoryOpts{
 			Include: []string{
 				"tests/",
@@ -46,7 +41,12 @@ func main() {
 		"yarn", "install",
 	})
 
+	secret_user := client.SetSecret("utilities-username-secret", os.Getenv("SEATTLE_UTILITIES_USERNAME"))
+	secret_pass := client.SetSecret("utilities-password-secret", os.Getenv("SEATTLE_UTILITIES_PASSWORD"))
+
 	out, err := runner.WithEnvVariable("CACHEBUSTER", time.Now().String()).
+		WithSecretVariable("SEATTLE_UTILITIES_USERNAME", secret_user).
+		WithSecretVariable("SEATTLE_UTILITIES_PASSWORD", secret_pass).
 		WithExec([]string{"npx", "playwright", "test"}).
 		Stderr(ctx)
 	if err != nil {
